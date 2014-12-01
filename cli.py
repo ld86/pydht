3a2d18738baf746290c4f1e9c31654fd7d39294f
Bogdan Melnik <ld@hackerdom.ru>
2014-12-02 03:16:36 +0500

    First pieces of cli tool

--- cli.py
+++ cli.py
@@ -1,7 +1,6 @@
-if __name__ == "__main__":
-    bootstrap = [("ts", 6881), ("192.168.1.100", 6881)]
-    node = Node("0.0.0.0", 6881, bootstrap)
-    node.start()
-    while True:
-        sleep(60)
+from node import Node
 
+if __name__ == "__main__":
+    node = Node()
+    node.protocol.send_find_node(("127.0.0.1", 6881), node.nid)
+    print(node.recv())
