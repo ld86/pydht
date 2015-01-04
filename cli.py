e0e4cdf261b48b5f5031fe8d71678115894e620c
Bogdan Melnik <ld@hackerdom.ru>
2015-01-05 04:05:17 +0500

    Mock get and store from cli

--- cli.py
+++ cli.py
@@ -1,6 +1,9 @@
-from node import Node
+def get(key):
+    pass
+
+
+def store(key, value):
+    pass
 
 if __name__ == "__main__":
-    node = Node()
-    node.protocol.send_find_node(("127.0.0.1", 6881), node.nid)
-    print(node.recv())
+    pass
