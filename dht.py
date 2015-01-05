fc4b3e519b3f9286f0bce5d7c330c6e136391a5a
Bogdan Melnik <ld@hackerdom.ru>
2015-01-06 00:45:12 +0500

    Move main to separate file

---
+++ dht.py
@@ -0,0 +1,5 @@
+from node import Node
+
+if __name__ == '__main__':
+    node = Node(port=6881)
+    node.serve()
--- node.py
+++ node.py
@@ -10,6 +10,7 @@ from logger import log
 
 
 class Node:
+    bootstrap_nodes = [("ts", 6881), ("192.168.1.100", 6881)]
 
     def __init__(self, ip=None, port=None, nid=None):
         ip, port = address = (ip if ip is not None else "0.0.0.0", port if port is not None else randint(40000, 50000))
@@ -37,12 +38,6 @@ class Node:
         self.protocol.handle_request(message, address)
 
     def serve(self):
+        self.protocol.bootstrap(Node.bootstrap_nodes)
         while True:
             self.recv_and_handle()
-
-
-if __name__ == "__main__":
-    bootstrap_nodes = [("ts", 6881), ("192.168.1.100", 6881)]
-    node = Node(port=6881)
-    node.protocol.bootstrap(bootstrap_nodes)
-    node.serve()
