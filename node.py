c610df30573e5d4eb956963bfe3089fedaf01d06
Bogdan Melnik <ld@hackerdom.ru>
2015-01-06 01:06:34 +0500

    Start node in separate thread

--- node.py
+++ node.py
@@ -4,17 +4,23 @@ from random import randint
 from utils import random_id
 from protocol import Protocol
 from logger import log
+from threading import Thread
 
 
-class Node:
+class Node(Thread):
     bootstrap_nodes = [("ts", 6881), ("192.168.1.100", 6881)]
 
     def __init__(self, ip=None, port=None, nid=None):
-        ip, port = address = (ip if ip is not None else "0.0.0.0", port if port is not None else randint(40000, 50000))
+        super(Node, self).__init__()
+        self.daemon = True
+
+        ip, port = address = (ip if ip is not None else "0.0.0.0",
+                              port if port is not None else randint(40000, 50000))
         nid = nid if nid is not None else random_id()
         self.address = NodeAddress(nid, ip, port)
 
         self.ufd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.ufd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.ufd.bind(address)
 
         self.table = Table(self.address)
@@ -34,7 +40,7 @@ class Node:
         (message, address) = self.recv()
         self.protocol.handle_request(message, address)
 
-    def serve(self):
+    def run(self):
         self.protocol.bootstrap(Node.bootstrap_nodes)
         while True:
             self.recv_and_handle()
