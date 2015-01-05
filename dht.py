b81d9d5d04654477d5b47fd538f9edef623657ba
Bogdan Melnik <ld@hackerdom.ru>
2015-01-06 01:07:09 +0500

    Http status page

--- dht.py
+++ dht.py
@@ -1,5 +1,15 @@
 from node import Node
+from flask import Flask, render_template
+
+port = 6881
+node = Node(port=port)
+status_app = Flask(__name__)
+
+
+@status_app.route('/')
+def status():
+    return render_template("status.html", node=node)
 
 if __name__ == '__main__':
-    node = Node(port=6881)
-    node.serve()
+    node.start()
+    status_app.run(host=node.address.ip, port=port + 1)
